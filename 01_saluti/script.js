5e53533b87a07d627667578e88470b717b6e0c4f
christiantondo <christiantondo1@gmail.com>
2025-11-08 09:46:22 +0100

    01_saluti completata

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -3,11 +3,15 @@
 const userName = 'Mario';
 
 
-// Dichiara la funzione qui.
 
+
+// Dichiara la funzione qui.
+function printName(string) {
+    alert(`Ciao ${string} benvenuto su questo sito web`)
+    console.log(`L'utente ${string} ha effettuato l'accesso al sito` )
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+printName("Mario")
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
