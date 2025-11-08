c150fbd01375c5ae90ba04ad56f9e1aa9641eeb1
christiantondo <christiantondo1@gmail.com>
2025-11-08 10:57:44 +0100

    02_iniziali completata

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,10 +1,18 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-
+let namesNewArray = ""
 
 // Dichiara la funzione qui.
-
+function nameInitials(array) {
+    const initialsArray = []
+    for (let i = 0; i < array.length; i++) {
+        const name = array[i];
+        const initial = name[0];
+        initialsArray.push(initial);
+        }
+        return initialsArray;
+    }
 
 // Invoca la funzione qui e stampa il risultato in console
 
