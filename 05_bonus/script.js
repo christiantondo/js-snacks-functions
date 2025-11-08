aeba2bb234001078e273ab124a1294f4b87d8937
christiantondo <christiantondo1@gmail.com>
2025-11-08 10:10:35 +0100

    bozza 05_bonus

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -9,10 +9,18 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
-
-
+function currentDateGreetings(string) {
+    const currentDate = new Date().getHours()
+    if (currentDate < 13) {
+        alert(`Buongiorno ${string}`);
+    } else if (currentDate < 17) {
+        alert(`Buon pomeriggio ${string}`);
+    } if (currentDate > 17) {
+        alert(`Buonasera ${string}`);
+    }
+}
 // Invoca la funzione qui e stampa il risultato in console
-
+currentDateGreetings("Mario")
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
