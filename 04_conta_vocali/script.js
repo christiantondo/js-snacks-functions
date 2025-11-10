09628de9314595db99726dc5478d9c7b75831600
christiantondo <christiantondo1@gmail.com>
2025-11-10 03:57:03 +0100

    04_conta_vocali completato

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -2,12 +2,23 @@
 
 const word = 'javascript';
 
-
 // Dichiara la funzione qui.
+const countVowels = (string) => {
 
+    let result = 0;
+
+    for (let i = 0; i < string.length; i++) {
+        const thisChar = string.charAt(i).toLowerCase();
+
+        if (thisChar == "a" || thisChar == "e" || thisChar == "i" || thisChar == "o" || thisChar == "u") {
+            result++;
+        }
+    }
+    return result;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(countVowels(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
