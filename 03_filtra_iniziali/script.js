36f6cfb77e06c40e86558f412f084edd9c0e9f49
christiantondo <christiantondo1@gmail.com>
2025-11-10 03:47:54 +0100

    03_filtra_iniziali completato

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,29 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+const filterByInitial = (array, letter) => {
+    const results = []
+    const letterToCheck = letter.toLowerCase();
+
+    for (let i = 0; i < array.length; i++) {
+        const thisWord = array[i];
+        const thisInitial = thisWord[0].toLowerCase();
+
+        console.log(i, `Lavorazione di "${thisWord}". Inizia con "${thisInitial}".`);
+
+        if (thisInitial === letterToCheck) {
+            results.push(thisWord);
+        }
+
+        console.log(i, "Stato attuale dell'array: ", results);
+    }
+
+    return results;
+};
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(filterByInitial(names, "a"));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
